baee7ad70472e221bba3ff7337c9f638587ae00c
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-02-03 15:14:45 +0200

    Lab1 done!

--- src/main/java/Lab1/Lab1.java
+++ src/main/java/Lab1/Lab1.java
@@ -1,4 +1,61 @@
 package Lab1;
+import java.util.Scanner;
 
 public class Lab1 {
+
+    public static void calculateDistanceBetweenCars() {
+        System.out.println("TASK 1");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter speed of first car: ");
+        int speed1 = scanner.nextInt();
+        System.out.println("Enter speed of second car: ");
+        int speed2 = scanner.nextInt();
+        System.out.println("Enter time in hours: ");
+        int hours = scanner.nextInt();
+        System.out.println("Enter initial distance between cars: ");
+        int initialDistance = scanner.nextInt();
+
+        int resultDistance = Math.max(0, initialDistance - (speed1 + speed2) * hours);
+
+        System.out.println("The result distance between cars is: " + resultDistance);
+    }
+
+    public static void calculateY() {
+        System.out.println("\nTASK 2");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an a, b, x:");
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double x = scanner.nextDouble();
+
+        double y = Math.pow(x, a / b) - Math.pow((x + b) / a, 1.0 / 3);
+        System.out.println("The value of y is: " + y);
+    }
+
+    public static void canQueenMove() {
+        System.out.println("\nTASK 3");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter coordinates: ");
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+
+        boolean canMove = (x1 == x2) || (y1 == y2) || (Math.abs(x1 - x2) == Math.abs(y1 - y2));
+        if (canMove) {
+            System.out.println("Queen can be moved at " + x2 + ", " + y2);
+        } else {
+            System.out.println("Queen can't be moved at " + x2 + ", " + y2);
+        }
+    }
+
+
+    public static void lab_demonstration() {
+        calculateDistanceBetweenCars();
+        calculateY();
+        canQueenMove();
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
+import Lab1.Lab1;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of lab ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                Lab1.lab_demonstration();
+                break;
+            case 2:
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
     }
 }
