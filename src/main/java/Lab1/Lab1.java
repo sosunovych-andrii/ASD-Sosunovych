ce1aa46380e8aff63e08c3d2d47779f928d170c6
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-02-14 20:09:24 +0200

    Lab1 fixed

--- src/main/java/Lab1/Lab1.java
+++ src/main/java/Lab1/Lab1.java
@@ -8,51 +8,62 @@ public class Lab1 {
         System.out.println("TASK 1");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter speed of first car: ");
-        int speed1 = scanner.nextInt();
-        System.out.println("Enter speed of second car: ");
-        int speed2 = scanner.nextInt();
-        System.out.println("Enter time in hours: ");
-        int hours = scanner.nextInt();
-        System.out.println("Enter initial distance between cars: ");
-        int initialDistance = scanner.nextInt();
+        try {
+            System.out.println("Enter speed of first car: ");
+            int speed1 = scanner.nextInt();
+            System.out.println("Enter speed of second car: ");
+            int speed2 = scanner.nextInt();
+            System.out.println("Enter time in hours: ");
+            int hours = scanner.nextInt();
+            System.out.println("Enter initial distance between cars: ");
+            int initialDistance = scanner.nextInt();
 
-        int resultDistance = Math.max(0, initialDistance - (speed1 + speed2) * hours);
+            int resultDistance = Math.max(0, initialDistance - (speed1 + speed2) * hours);
 
-        System.out.println("The result distance between cars is: " + resultDistance);
+            System.out.println("The result distance between cars is: " + resultDistance);
+        } catch (Exception e) {
+            System.out.println("Invalid input! Please enter numeric values.");
+        }
     }
 
     public static void calculateY() {
         System.out.println("\nTASK 2");
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        System.out.println("Enter an a, b, x:");
-        double a = scanner.nextDouble();
-        double b = scanner.nextDouble();
-        double x = scanner.nextDouble();
+        try {
+            System.out.println("Enter an a, b, x:");
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            double x = scanner.nextDouble();
 
-        double y = Math.pow(x, a / b) - Math.pow((x + b) / a, 1.0 / 3);
-        System.out.println("The value of y is: " + y);
+            double y = Math.pow(x, a / b) - Math.pow((x + b) / a, 1.0 / 3);
+            System.out.println("The value of y is: " + y);
+        } catch (Exception e) {
+            System.out.println("Invalid input! Please enter numeric values.");
+        }
     }
 
     public static void canQueenMove() {
         System.out.println("\nTASK 3");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter coordinates: ");
-        int x1 = scanner.nextInt();
-        int y1 = scanner.nextInt();
-        int x2 = scanner.nextInt();
-        int y2 = scanner.nextInt();
+        try {
+            System.out.println("Enter coordinates: ");
+            int x1 = scanner.nextInt();
+            int y1 = scanner.nextInt();
+            int x2 = scanner.nextInt();
+            int y2 = scanner.nextInt();
 
-        boolean canMove = (x1 == x2) || (y1 == y2) || (Math.abs(x1 - x2) == Math.abs(y1 - y2));
-        if (canMove) {
-            System.out.println("Queen can be moved at " + x2 + ", " + y2);
-        } else {
-            System.out.println("Queen can't be moved at " + x2 + ", " + y2);
+            boolean canMove = (x1 == x2) || (y1 == y2) || (Math.abs(x1 - x2) == Math.abs(y1 - y2));
+            if (canMove) {
+                System.out.println("Queen can be moved at " + x2 + ", " + y2);
+            } else {
+                System.out.println("Queen can't be moved at " + x2 + ", " + y2);
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input! Please enter integer values.");
         }
     }
-
 
     public static void lab_demonstration() {
         calculateDistanceBetweenCars();
